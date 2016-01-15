fc2c6fe18ad8951ede4e74e04eab17fcf30c500e
ansonchoi <anson.github@gmail.com>
2016-01-15 16:44:55 +0800

    testing

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is the main starting point of our project");
 		System.out.println("Go update");
-		System.out.println("HIHI");
+		System.out.println("HI");
 	}
 
 }
