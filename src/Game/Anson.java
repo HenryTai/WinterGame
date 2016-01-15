80b5f84728573a1278c3585d92c027573b0592cc
ansonchoi <anson.github@gmail.com>
2016-01-15 21:06:17 +0800

    added Anson.java

---
+++ src/Game/Anson.java
@@ -0,0 +1,11 @@
+package Game;
+
+public class Anson {
+
+	public static void main(String[] args) {
+		System.out.println("This is the main starting point of our project");
+		System.out.println("Go update");
+		System.out.println("HIHI");
+	}
+
+}
